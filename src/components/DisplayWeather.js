2754f066ae50dc2c05f8eef41f131b4e3b368c93
mattgeneuxDS <135951558+mattgeneuxDS@users.noreply.github.com>
2023-06-12 16:16:57 +0100

    more

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -2,28 +2,45 @@ import React from "react";
 import "./displayweather.css";
 const DisplayWeather = (props) => {
   const { data, unit } = props;
-
+  const unitDisplay = unit === "metric" ? "C" : unit === "imperial" ? "F" : "K";
   const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
   return (
     <div className="displayweather">
       <div className="maincard">
-        <span className="cardtitle">
+        <div className="cardtitle">
           {data.name}, {data.sys.country}
-        </span>
+        </div>
 
-        <span className="cardsubtitle">
+        <div className="cardsubtitle">
           As of {new Date().toLocaleDateString()}
-        </span>
+        </div>
         <h1>
           {Math.floor(data.main.temp)}
           <sup>o</sup>
-          {unit === "metric" ? "C" : unit === "imperial" ? "F" : "K"}
+          {unitDisplay}
         </h1>
 
-        <span className="weather-main">
-          {data.weather[0].main}
-          <img src={iconUrl} className="weather-icon" alt="weather icon" />
-        </span>
+        <div className="weather-main">{data.weather[0].main}</div>
+        <img src={iconUrl} className="weather-icon" alt="weather icon" />
+        <div className="weather-description">{data.weather[0].description}</div>
+      </div>
+
+      <div className="weatherdetails">
+        <div className="section1">
+          <table>
+            <tr>
+              <td>
+                <h4>High/Low</h4>
+              </td>
+              <td>
+                <span>
+                  {Math.floor(data.main.temp_max)} /{" "}
+                  {Math.floor(data.main.temp_min)} {unitDisplay}
+                </span>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   );
